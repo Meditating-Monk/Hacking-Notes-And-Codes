d0958598f0fb3d42fa6f1939a2ebbf4b19a48c57
Ujjwal Kumar Singh <singh.ujjwal2394@gmail.com>
2026-02-14 12:21:41 +0530

    Created authentication and setting master password and updating it.

--- passman/passman.py
+++ passman/passman.py
@@ -2,20 +2,72 @@
 
 #modules and package imports
 import sys
+import hashlib
+import getpass
+import json
+
+
+
+
+
+#global veriables
+
+
+auth_bool=False
+
+
+#Password creation 
+def change_master_passwd():
+    master_passwd = getpass.getpass('Create your Password: ')
+    temp_verification = getpass.getpass('Enter above Password: ')
+    if master_passwd == temp_verification:
+        temp_data = {
+            "master_passwd_hash": hashlib.sha512(master_passwd.encode("utf-8")).hexdigest()
+        }
+        with open("./config.json",'w') as f:
+            json.dump(temp_data,f,indent=4)
+
+        print("Master Password Set")
+        global auth_bool
+        auth_bool = True
+    else:
+        print("The passwords didn't match.")
+
+
+
+
 
 #Authentication function-will verify if master password is correct
 def authentication():
-    print('for authentication')
+    for i in range(1,4):
+        input_passwd = getpass.getpass('Input Your Password: ')
+        with open("./config.json","r")as f:
+            temp_data = json.load(f)
+        if temp_data["master_passwd_hash"] == hashlib.sha512(input_passwd.encode("utf-8")).hexdigest():
+            global auth_bool
+            auth_bool = True
+            print('Welcome !')
+            break
+        else :
+            print('Wrong Password.')
+            print('You have ',(3-i),' more attempts left')
 
-#main menu for all options provided to manage passwords    
+
+
+
+#main menu for all options provided t:o manage passwords    
 def menu():
-    print('--======PASSMAN======--')
-    print('Add Password         a')
-    print('View Password        v')
-    print('Update Password      u')
-    print('Delete Password      d')
-    print('List All             l')
-    print('Exit                 e')
+    print('\n--======PASSMAN======--')
+    print('Add Password                                 a')
+    print('View Password                                v')
+    print('Update Password                              u')
+    print('Delete Password                              d')
+    print('List All                                     l')
+    print('Change Master Password                       c')
+    print('Exit                                         e')
+    
+
+
 
 #codes for all functions    
 def add_passw():
@@ -34,37 +86,50 @@ def list_passw():
     print('for listing')
 
 def exit():
-    print('to exit menu')
     sys.exit(0)
 
 #main function code
 def main():
-    while True:
-        
-        menu()
-        
-        selected_option=input('Select option: ')
 
-        if selected_option=='a':
-            add_passw()
-
-        elif selected_option=='v':
-            view_passw()
-
-        elif selected_option=='u':
-            update_passw()
-
-        elif selected_option=='d':
-            delete_passw()
-
-        elif selected_option=='l':
-            list_passw()
-
-        elif selected_option=='e':
-            exit()
-
+    with open("./config.json","r") as f:
+        if not f.read(1):
+            change_master_passwd()
         else:
-            print('Invalid option.')
+            authentication()
+
+
+    while True:       
+
+        if auth_bool == True:
+            menu()
+        
+            selected_option=input('Select option: ')
+
+            if selected_option=='a':
+                add_passw()
+
+            elif selected_option=='v':
+                view_passw()
+
+            elif selected_option=='u':
+                update_passw()
+
+            elif selected_option=='d':
+                delete_passw()
+
+            elif selected_option=='l':
+                list_passw()
+
+            elif selected_option =='c':
+                change_master_passwd()
+
+            elif selected_option=='e':
+                exit()
+
+            else:
+                print('Invalid option.')
+        else:
+            exit()
 
 if __name__=='__main__':
     main()
